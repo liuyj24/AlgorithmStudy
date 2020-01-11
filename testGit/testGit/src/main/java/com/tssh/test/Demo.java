08d244d6c6045c2db46bc366d12562d5ae3a47d6
liuyj24 <liuyijun2017@email.szu.edu.cn>
2020-01-11 14:07:53 +0800

    edit on branch hot_fix

--- testGit/testGit/src/main/java/com/tssh/test/Demo.java
+++ testGit/testGit/src/main/java/com/tssh/test/Demo.java
@@ -3,5 +3,6 @@ package com.tssh.test;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("master...");
+        System.out.println("hot fix...");
     }
 }
