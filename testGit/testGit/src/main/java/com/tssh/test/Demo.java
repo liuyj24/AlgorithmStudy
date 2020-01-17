d6df8eec0be555a434e041061919a4d5f5c46dc3
liuyj24 <liuyijun2017@email.szu.edu.cn>
2020-01-17 15:55:45 +0800

    test

--- testGit/testGit/src/main/java/com/tssh/test/Demo.java
+++ testGit/testGit/src/main/java/com/tssh/test/Demo.java
@@ -3,6 +3,5 @@ package com.tssh.test;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("master...");
-        System.out.println("hot fix...");
     }
 }
