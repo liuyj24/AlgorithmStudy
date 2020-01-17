c45fae3710beb79621a9037658aa27af7e92ad00
liuyj24 <liuyijun2017@email.szu.edu.cn>
2020-01-17 14:26:57 +0800

    test amend

---
+++ testGit/testGit/src/main/java/com/tssh/dao/UserMapper.java
@@ -0,0 +1,15 @@
+package com.tssh.dao;
+
+import com.tssh.Bean.User;
+
+public class UserMapper {
+
+    public User findAll(){
+        User user = new User();
+        return user;
+    }
+
+    public void test(){
+
+    }
+}
